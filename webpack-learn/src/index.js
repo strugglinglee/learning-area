c6e5a5f319e86a7933089e71d1704b016d7958b1
strugglinglee <strugglinglee@163.com>
2021-07-29 11:05:26 +0800

    learn(webpack):lazyload

--- webpack-learn/src/index.js
+++ webpack-learn/src/index.js
@@ -1,18 +1,35 @@
-// import _ from 'lodash';
-import printMe from './print';
+import _ from 'lodash';
+// import printMe from './print';
 import './style.css';
-import { cube } from './math';
+// import { cube } from './math';
 
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
 
-async function getComponent() {
+function component() {
   var element = document.createElement('div');
-  const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
+  // const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
   // console.log(_.default.join)
 
-  element.innerHTML = _.default.join(['Hello', 'webpack'], ' ');
+  var button = document.createElement('button');
+  var br = document.createElement('br');
+
+  button.innerHTML = 'Click me and look at the console!';
+
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.appendChild(br);
+  element.appendChild(button);
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = (e) =>
+    import(/* webpackChunkName: "print" */ './print').then((module) => {
+      var print = module.default;
+
+      print();
+    });
 
   return element;
 }
@@ -20,9 +37,9 @@ async function getComponent() {
 // let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
 // document.body.appendChild(element);
 
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+// getComponent().then((component) => {
+document.body.appendChild(component());
+// });
 
 // if (module.hot) {
 //   module.hot.accept('./print.js', function () {
