a8565b1876e220daf4462739a3da5618e2cea813
strugglinglee <strugglinglee@163.com>
2022-01-27 18:15:28 +0800

    learn(算法): 链表应用

--- algorithm/draft/listNode/1.js
+++ algorithm/draft/listNode/1.js
@@ -38,7 +38,7 @@ const mergeTwoLists = function (l1, l2) {
   // cur 这里就是咱们那根“针”
   let cur = head;
 
-  console.log('init cur:', cur, ',head:', head);
+  // console.log('init cur:', cur, ',head:', head);
   // “针”开始在 l1 和 l2 间穿梭了
   while (l1 && l2) {
     // 如果 l1 的结点值较小
@@ -47,11 +47,13 @@ const mergeTwoLists = function (l1, l2) {
       cur.next = l1;
       // l1 指针向前一步
       l1 = l1.next;
+      // console.log('cur.next', cur.next, 'l1', l1);
     } else {
       // l2 较小时，串起 l2 结点
       cur.next = l2;
       // l2 向前一步
       l2 = l2.next;
+      // console.log('cur.next', cur.next, 'l2', l2);
     }
 
     // “针”在串起一个结点后，也会往前一步
@@ -62,9 +64,37 @@ const mergeTwoLists = function (l1, l2) {
   // 处理链表不等长的情况
   cur.next = l1 !== null ? l1 : l2;
 
-  console.log('cur:', cur, 'head:', head);
+  // console.log('cur:', cur, 'head:', head);
   // 返回起始结点
   return head.next;
 };
 
-mergeTwoLists(L1,L2)
+// mergeTwoLists(L1, L2);
+
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ const reverseList = function (head) {
+  // 初始化前驱结点为 null
+  let pre = null;
+  // 初始化目标结点为头结点
+  let cur = head;
+  // 只要目标结点不为 null，遍历就得继续
+  while (cur !== null) {
+    // 记录一下 next 结点
+    let next = cur.next;
+    // 反转指针
+    cur.next = pre;
+    // pre 往前走一步
+    pre = cur;
+    // cur往前走一步
+    cur = next;
+    console.log(pre)
+  }
+  // 反转结束后，pre 就会变成新链表的头结点
+  return pre;
+};
+
+reverseList(L1)
